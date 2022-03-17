f7453f68bca2581864a49dab24462a1a8436f18c
Taichi Kudo <t.kudou.sys21@morijyobi.ac.jp>
2022-03-17 12:26:32 +0900

    add .gitignore

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+            double kg,m,bmi;
+            m = 3;
+            kg = 150;
+            bmi = kg/(m*m);
+            System.out.println(bmi);
+    }
+}
